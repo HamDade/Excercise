3567e1217ba9569f1df6fec52f570871ff0d50b2
DadeWork <116323289+HamDade@users.noreply.github.com>
2023-08-31 10:31:50 +0100

    made overly complicated(?) date outputs, different formats

--- 3-Current Date in formats/script.js
+++ 3-Current Date in formats/script.js
@@ -1,4 +1,10 @@
 const date = new Date();
 console.log(date);
-let createButton = document.createElement("div");
-document.getElementById("thing").after(createButton);
+const month = date.toLocaleString("en-GB", { month: "2-digit" });
+const day = date.toLocaleString("en-GB", { day: "2-digit" });
+const year = date.toLocaleString("en-GB", { year: "numeric" });
+document
+  .getElementById("thing")
+  .after(
+    `${month}-${day}-${year}, ${month}/${day}/${year} or ${day}-${month}-${year}, ${day}/${month}/${year}`
+  );
